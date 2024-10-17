4a2f406a44c87569fd099974925370b906a8f876
Orlan Castro <orlancssst@gmail.com>
2024-10-16 21:42:25 -0500

    a file has been changed on my Home PC

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          This file has been changed on my Home PC.
         </p>
       </div>
       <p className="read-the-docs">
